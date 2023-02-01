c725e07de7df683fa1430de38e5fb4c98dd9173a
GiedriusKazlauskas <Giedrius.Ano@gmail.com>
2023-02-01 14:55:58 +0200

    Atnaujinta.

--- ND/JavaScript/js.js
+++ ND/JavaScript/js.js
@@ -1,12 +1,20 @@
 
-// pirma užduotis.
+/* pirma užduotis.
+
+Sukurti du kintamuosius. Jiems priskirti savo mylimo aktoriaus vardą ir pavardę kaip stringus (Jonas Jonaitis).
+ Atspausdinti trumpesnį stringą.*/
+
 
 var Name= "Travis";
 var lName= " Fimmel";
 const full=Name+lName;
 console.log(full);
 
-// antra užduotis.
+/* antra užduotis.
+
+ Sukurkite 4 kintamuosius, kurie saugotų jūsų vardą, pavardę, gimimo metus ir šiuos metus (nebūtinai tikrus). Parašykite kodą, kuris pagal gimimo metus paskaičiuotų jūsų amžių ir naudodamas vardo ir pavardės kintamuosius atspausdintų tokį sakinį :
+ "Aš esu Vardenis Pavardenis. Man yra XX metai(ų)".*/
+
 
 var vardas="Java";
 var pavardė=" Script";
@@ -17,7 +25,12 @@ var age=tYear - year;
 var string="Aš esu "+vardas + pavardė + "\." + " Man yra "+ age + " metai.";
 console.log(string);
 
-// trečia užduotis.
+/* trečia užduotis.
+
+Sukurti du kintamuosius. Jiems priskirti savo mylimo aktoriaus vardą ir pavardę kaip stringus.
+ Sukurti trečią kintamąjį ir jam priskirti stringą, sudarytą iš trijų paskutinių vardo ir pavardės kintamųjų raidžių.
+  Jį atspausdinti.*/
+
 
 var n="Jackie";
 var p="Chan";
@@ -25,13 +38,21 @@ var np="kiehan";
 
 console.log(np);
 
-// Ketvirta užduotis.
+/* Ketvirta užduotis.
+
+Sukurti kintamąjį su stringu: “Once upon a time in hollywood”.
+ Jame visas “o” (didžiąsias ir mažąsias) pakeisti žvaigždutėm “*”.  Rezultatą atspausdinti.*/
+
 
 var stringas="Once upon a time in hollywood";
 var pakeisti=stringas.replace("Once upon a time in hollywood","*nce up*n a time in h*llyw**d");
 console.log(pakeisti);
 
-// Penkta užduotis. 
+/* Penkta užduotis.
+
+Sukurkite keturis kintamuosius kuriems sugeneruokite atsitiktines reikšmes nuo 0 iki 2.
+ Suskaičiuokite kiek yra nulių, vienetų ir dvejetų. */
+
 
 
 function rand(min, max) {
@@ -45,7 +66,47 @@ var antras=rand(0,2);
 var trečias=rand(0,2);
 var ketvirtas=rand(0,2);
 
+var nuliai=0;
+var vienetai=0;
+var dvejetai=0;
+
+if (pirmas===0){
+    nuliai++;
+}else if(pirmas===1){
+    vienetai++;
+}else{
+    dvejetai++;
+}
+
+if (antras===0){
+    nuliai++;
+}else if(antras===1){
+    vienetai++;
+}else{
+    dvejetai++;
+}
+
+if (trečias===0){
+    nuliai++;
+}else if(trečias===1){
+    vienetai++;
+}else{
+    dvejetai++;
+}
+
+if (ketvirtas===0){
+    nuliai++;
+}else if(ketvirtas===1){
+    vienetai++;
+}else{
+    dvejetai++;
+}
+
 console.log(pirmas,antras,trečias,ketvirtas);
+
+console.log("Nuliai = " +(nuliai));
+console.log("vienetai = " +(vienetai));
+console.log("dvejetai = " +(dvejetai));
 
 
 
